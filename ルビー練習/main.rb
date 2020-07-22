8db041f4b5db95602bb1eda3e3720b53359a3396
Yusuke-kawano <65767852+Yusuke-kawano@users.noreply.github.com>
2020-07-22 22:13:53 +0900

    Update and rename Ruby練習/main.rb to ルビー練習/main.rb

--- ルビー練習/main.rb
+++ ルビー練習/main.rb
@@ -2,11 +2,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 require './key'
-
-endPoint  = 'https://api.coin.z.com/public'
-path      = '/v1/trades?symbol=BTC&page=1&count=10'
-
-uri = URI.parse(endPoint + path)
+t (endoint
 req = Net::HTTP::Get.new(uri.to_s)
 
 response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') {|http|
